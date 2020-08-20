7c8c647035f90b618bf430e00acff0fb074f9e3f
Ashish Maharjan <amaharjan1032@gmail.com>
2020-08-20 23:47:14 +0545

    Error handling and alerts done

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,22 +1,35 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
 
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
 import Header from "./layout/header";
 import Dashboard from "./leads/Dashboard";
+import Alert from "./layout/Alert";
 
 import { Provider } from "react-redux";
 import store from "../store";
+
+//Alert options
+const alertOptions = {
+  timeout: 3000,
+  position: "top center",
+};
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <div className="container">
-            <Dashboard />
-          </div>
-        </Fragment>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Fragment>
+            <Header />
+            <Alert />
+            <div className="container">
+              <Dashboard />
+            </div>
+          </Fragment>
+        </AlertProvider>
       </Provider>
     );
   }
